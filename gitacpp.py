13f21c50c79aec0f673c3cd11427625ac3b1a14e
feedme2 <pham.alan10@gmail.com>
2015-09-13 15:31:53 -0700

    added comments and add pull command

--- gitacpp.py
+++ gitacpp.py
@@ -1,10 +1,15 @@
 import os
 
+#does add command.
 os.system("git add *")
 
+#asks for input.
 commitmsg = raw_input('Enter commit message: ')
-
 print(commitmsg)
 
+#does commit.
 os.system("git commit -m \"" + commitmsg + "\"")
+
+#does pull and push.
+os.system("git pull origin master")
 os.system("git push origin master")
