751e19715526fcef4b31f5a6c3d258bdb437a18d
feedme2 <pham.alan10@gmail.com>
2015-09-13 15:29:24 -0700

    testing the python script

--- gitacpp.py
+++ gitacpp.py
@@ -1,3 +1,10 @@
 import os
 
 os.system("git add *")
+
+commitmsg = raw_input('Enter commit message: ')
+
+print(commitmsg)
+
+os.system("git commit -m \"" + commitmsg + "\"")
+os.system("git push origin master")
