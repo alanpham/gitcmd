f86bb8bc60d30e5d8c4560b8da9005cb0a9ffab6
Alan Pham <pham.alan10@gmail.com>
2016-04-16 10:25:19 -0700

    adds begin.py, opens up browser with common links that I open up in the morning

---
+++ begin.py
@@ -0,0 +1,8 @@
+import os
+
+#Opens reddit.
+os.system("python -m webbrowser -t \"https://www.reddit.com\"")
+#Opens facebook.
+os.system("python -m webbrowser -t \"https://www.facebook.com\"")
+#Opens AndroidCentral
+os.system("python -m webbrowser -t \"https://www.androidcentral.com\"")
