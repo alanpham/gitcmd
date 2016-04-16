ee893b03032ab60b0191c163569c95a689fe986a
Alan Pham <pham.alan10@gmail.com>
2016-04-16 10:49:47 -0700

    changed facebook to messenger since I use that more, added ESPN link

--- begin.py
+++ begin.py
@@ -2,7 +2,9 @@ import os
 
 #Opens reddit.
 os.system("python -m webbrowser -t \"https://www.reddit.com\"")
-#Opens facebook.
-os.system("python -m webbrowser -t \"https://www.facebook.com\"")
+#Opens Messenger.
+os.system("python -m webbrowser -t \"https://www.messenger.com\"")
 #Opens AndroidCentral
 os.system("python -m webbrowser -t \"https://www.androidcentral.com\"")
+#Opens ESPN
+os.system("python -m webbrowser -t \"https://www.espn.go.com\"")
