cf9a4a87f4f1480138abd7e723073fd9550e9dc5
Alan Pham <pham.alan10@gmail.com>
2016-04-16 22:38:41 -0700

    added Macrumors back, copy and pasted begin.py to automator

--- begin.py
+++ begin.py
@@ -1,10 +1,6 @@
 import os
 
-#Opens reddit.
 os.system("python -m webbrowser -t \"https://www.reddit.com\"")
-#Opens Messenger.
 os.system("python -m webbrowser -t \"https://www.messenger.com\"")
-#Opens AndroidCentral
+os.system("python -m webbrowser -t \"https://www.macrumors.com\"")
 os.system("python -m webbrowser -t \"https://www.androidcentral.com\"")
-#Opens ESPN
-os.system("python -m webbrowser -t \"https://www.espn.go.com\"")
