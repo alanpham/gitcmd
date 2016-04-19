2942e226144f2612d246048b0b37c02923043aa2
Alan Pham <pham.alan10@gmail.com>
2016-04-19 15:04:52 -0700

    adds gmail link

--- begin.py
+++ begin.py
@@ -1,6 +1,7 @@
 import os
 
 os.system("python -m webbrowser -t \"https://www.reddit.com\"")
+os.system("python -m webbrowser -t \"https://www.gmail.com\"")
 os.system("python -m webbrowser -t \"https://www.messenger.com\"")
 os.system("python -m webbrowser -t \"https://www.macrumors.com\"")
 os.system("python -m webbrowser -t \"https://www.androidcentral.com\"")
