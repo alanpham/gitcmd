6ee25cc5225f320eef65c4a4378fe93bc2f67a18
Alan Pham <pham.alan10@gmail.com>
2016-07-26 15:34:29 -0700

    updates README, adds help info based on command line arguments.

--- README.md
+++ README.md
@@ -2,18 +2,26 @@
 A collection of scripts that I wrote to make life easier.
 
 ## In this collection:
-* save.py
-* begin.py
+* `save.py`
+* `begin.py`
+* `ctl.sh`
 
 ### Save.py
+* This runs the commands needed to create a commit and to push to the remote branch.
+
 #### How to Use
-1. Run 'python save.py' in command line.
+1. Run `python save.py` in command line.
 2. Type in commit message.
 
 ### Begin.py
+* This just starts up websites that I normally view in the morning.
+
 #### How to Use
-1. Run 'python begin.py' in command line.
+1. Run `python begin.py` in command line.
+
+### ctl.sh
+* This is a bash script that will be used for the current koazie ec2 server. It handles the start/stop of `server.js` and the `mongod` service.
 
 
 ## Notes
-* Begin.py - working on getting it to a standalone application using py2app
+* begin.py is actually used through Automator fo the mac.
--- ctl.sh
+++ ctl.sh
@@ -1,2 +1,51 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    echo "usage:"
+    echo "     ./ctl.sh help"
+    echo "     ./ctl.sh (start|stop|restart) (location of server file)"
+    echo "     ./ctl.sh (start|stop|restart|status) mongod"
+    echo ""
+    echo "help       - this screen."
+    echo "start      - start the service."
+    echo "stop       - stop the service."
+    echo "restart    - restart or start the service."
+    echo "status     - show the status of the service."
+    exit 1
+fi
+
+cmd=$1
+case "$cmd" in
+    start )
+        echo "START"
+        subcmd=$2
+        case "$subcmd" in
+            test )
+                echo "TEST"
+                ;;
+        esac
+        ;;
+    stop )
+        echo "STOP"
+        subcmd=$2
+        case "$subcmd" in
+            test )
+                echo "TEST"
+                ;;
+        esac
+        ;;
+    \? )
+        ;;
+    help )
+        echo "usage:"
+        echo "     ./ctl.sh help"
+        echo "     ./ctl.sh (start|stop|restart) (location of server file)"
+        echo "     ./ctl.sh (start|stop|restart|status) mongod"
+        echo ""
+        echo "help       - this screen."
+        echo "start      - start the service."
+        echo "stop       - stop the service."
+        echo "restart    - restart or start the service."
+        echo "status     - show the status of the service."
+        ;;
+esac
