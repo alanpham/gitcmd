95b0535be2f9a8b969a8aeabae68fe196316775d
Alan Pham <pham.alan10@gmail.com>
2016-07-30 15:59:26 -0700

    removes market variable and places it into response url

--- GetBing.py
+++ GetBing.py
@@ -13,13 +13,11 @@ def createWallpaper(path):
 def setWallpaper(path):
   os.system("osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"" + path + "\"'")
 
-
-market = 'en-US'
 resolution = '1920x1080'
 BingSpotlightImgDir = expanduser("~") + '/Pictures/BingSpotlight/'
 nameWallpaper = 'wallpaper.jpg'
 
-response = urllib2.urlopen("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=" + market)
+response = urllib2.urlopen("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US")
 obj = json.load(response)
 url = (obj['images'][0]['urlbase'])
 url = 'http://www.bing.com' + url + '_' + resolution + '.jpg'
